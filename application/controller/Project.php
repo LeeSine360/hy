f59461fb394f28d4050995368313f9a568aef1a3
admin <249482000@qq.com>
2018-09-03 17:30:30 +0800

    修改

--- application/controller/Project.php
+++ application/controller/Project.php
@@ -1,9 +1,52 @@
 <?php
 namespace app\controller;
+
+use app\model\Project as Pro;
 use think\Controller;
 
-class Project extends Controller {
-	public function index() {
-		return $this->fetch();
-	}
+class Project extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function add()
+    {
+        $param = $_POST;
+        $pro   = Pro::create([
+            'name'       => $param['proName'],
+            'price'      => $param['proPrice'],
+            'start_time' => $param['proStart'],
+            'days'       => $param['proDays'],
+            'number'     => $param['proNumber'],
+            'remark'     => $param['proRemark'],
+        ]);
+
+        $id  = $pro->id;
+        $msg = $id > 0 ? "添加成功！" : "添加失败！";
+
+        return json(array('code' => $id, 'msg' => $msg));
+    }
+
+    public function query()
+    {
+        $list   = Pro::all();
+        $number = count($list);
+
+        $data = array();
+
+        foreach ($list as $key => $value) {
+            $data[] = array(
+                'proName'   => $value['name'],
+                'proPrice'  => $value['price'],
+                'proStart'  => date("Y-m-d", $value['start_time']),
+                'proDays'   => $value['days'],
+                'proNumber' => $value['number'],
+                'proRemark' => $value['remark']);
+        }
+
+        $return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
+        return json($return);
+    }
 }
---
+++ application/model/Project.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\model;
+use think\Model;
+
+class Project extends Model
+{
+	//protected $pk = 'uid';	//主键名称ID
+    //protected $table = 'think_user';// 设置当前模型对应的完整数据表名称     
+    //protected $connection = 'db_config';// 设置当前模型的数据库连接
+}
