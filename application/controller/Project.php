7566384c986b22ec662433a6b3631e41cac6ed6f
admin <249482000@qq.com>
2018-09-05 17:12:15 +0800

    edit

--- application/controller/Project.php
+++ application/controller/Project.php
@@ -14,19 +14,25 @@ class Project extends Controller
     public function add()
     {
         $param = $_POST;
-        $pro   = Pro::create([
-            'name'       => $param['proName'],
-            'price'      => $param['proPrice'],
-            'start_time' => $param['proStart'],
-            'days'       => $param['proDays'],
-            'number'     => $param['proNumber'],
-            'remark'     => $param['proRemark'],
-        ]);
+        if($param['proAC'] == 'project'){
+            $pro   = Pro::create([
+                'name'       => $param['proName'],
+                'price'      => $param['proPrice'],
+                'start_time' => $param['proStart'],
+                'days'       => $param['proDays'],
+                'number'     => $param['proNumber'],
+                'remark'     => $param['proRemark'],
+            ]);
 
-        $id  = $pro->id;
-        $msg = $id > 0 ? "添加成功！" : "添加失败！";
+            $id  = $pro->id;
+            $msg = $id > 0 ? "添加成功！" : "添加失败！";
+        }
+        if($param['proAC'] == 'bids'){
+            //{"proId":"10","bidsName":"234","bidsStart":"234","bidsDays":"234","bidsRemark":"234","proAC":"bids"}
+            //id, name, p_id, m_id, create_time, update_time
+        }
 
-        return json(array('code' => $id, 'msg' => $msg));
+        //return json(array('code' => $id, 'msg' => $msg));
     }
 
     public function query()
@@ -38,6 +44,7 @@ class Project extends Controller
 
         foreach ($list as $key => $value) {
             $data[] = array(
+                'proId'   => $value['id'],
                 'proName'   => $value['name'],
                 'proPrice'  => $value['price'],
                 'proStart'  => date("Y-m-d", $value['start_time']),
