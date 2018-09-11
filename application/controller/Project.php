41baf5553b9e83b31a5e46ab4dc363def105ef80
admin <249482000@qq.com>
2018-09-11 17:16:37 +0800

    修改

--- application/controller/Project.php
+++ application/controller/Project.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\controller;
 
+use app\model\Manager as Man;
 use app\model\Project as Pro;
 use think\Controller;
 
@@ -14,8 +15,8 @@ class Project extends Controller
     public function add()
     {
         $param = $_POST;
-        if($param['proAC'] == 'project'){
-            $pro   = Pro::create([
+        if ($param['proAC'] == 'project') {
+            $pro = Pro::create([
                 'name'       => $param['proName'],
                 'price'      => $param['proPrice'],
                 'start_time' => $param['proStart'],
@@ -27,7 +28,7 @@ class Project extends Controller
             $id  = $pro->id;
             $msg = $id > 0 ? "添加成功！" : "添加失败！";
         }
-        if($param['proAC'] == 'bids'){
+        if ($param['proAC'] == 'bids') {
             //{"proId":"10","bidsName":"234","bidsStart":"234","bidsDays":"234","bidsRemark":"234","proAC":"bids"}
             //id, name, p_id, m_id, create_time, update_time
         }
@@ -44,13 +45,33 @@ class Project extends Controller
 
         foreach ($list as $key => $value) {
             $data[] = array(
-                'proId'   => $value['id'],
+                'proId'     => $value['id'],
                 'proName'   => $value['name'],
                 'proPrice'  => $value['price'],
                 'proStart'  => date("Y-m-d", $value['start_time']),
                 'proDays'   => $value['days'],
                 'proNumber' => $value['number'],
                 'proRemark' => $value['remark']);
+        }
+
+        $return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
+        return json($return);
+    }
+    public function manager()
+    {
+        $param = $_GET;
+        $list   = Man::where('name','like',"%{$param['keyword']}%")->select();
+        $number = count($list);
+
+        $data = array();
+
+        foreach ($list as $key => $value) {
+            $data[] = array(
+                'value'    => $value['id'],
+                'name'     => $value['name'],
+                'selected' => "",
+                'disabled' => "",
+            );
         }
 
         $return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
