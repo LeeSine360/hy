570332c42fba90e039467b25296154833e1931aa
lee <249482000@qq.com>
2018-09-16 21:07:48 +0800

    修改

--- application/controller/Company.php
+++ application/controller/Company.php
@@ -1,9 +1,53 @@
 <?php
 namespace app\controller;
+
+use app\model\Company as Com;
 use think\Controller;
 
 class Company extends Controller {
 	public function index() {
 		return $this->fetch();
+	}
+	public function add() {
+		$param = $_POST;
+		$id = 0;
+		$msg = '';
+
+		$com = Com::create([
+			'name' => $param['comName'],
+			'bank_name' => $param['comAccountName'],
+			'account' => $param['comAccount'],
+			'phone' => $param['comPhone'],
+			'remark' => $param['remark'],
+		]);
+
+		$id = $com->id;
+		$msg = $id > 0 ? "添加成功！" : "添加失败！";
+
+		return json(array('code' => $id, 'msg' => $msg));
+	}
+
+	public function query() {
+		$param = $_GET;
+		$curr = $param['page'] <= 1 ? 1 : ($param['page'] - 1) * $param['limit'] + 1;
+		$limit = $param['limit'];
+		$list = Com::order('id', 'asc')->limit($curr, $limit)->select();
+		$listTotal = Com::all();
+		$total = count($listTotal);
+
+		$data = array();
+
+		foreach ($list as $key => $value) {
+			$data[] = array(
+				'comId' => $value['id'],
+				'comName' => $value['name'],
+				'comBankName' => $value['bank_name'],
+				'comAccount' => $value['account'],
+				'comPhone' => $value['phone'],
+				'comRemark' => $value['remark']);
+		}
+
+		$return = array('code' => 0, 'msg' => '', 'count' => $total, 'data' => $data);
+		return json($return);
 	}
 }
--- application/controller/Project.php
+++ application/controller/Project.php
@@ -4,77 +4,83 @@ namespace app\controller;
 use app\model\Manager as Man;
 use app\model\Project as Pro;
 use think\Controller;
+use think\Db;
 
-class Project extends Controller
-{
-    public function index()
-    {
-        return $this->fetch();
-    }
+class Project extends Controller {
+	public function index() {
+		return $this->fetch();
+	}
 
-    public function add()
-    {
-        $param = $_POST;
-        if ($param['proAC'] == 'project') {
-            $pro = Pro::create([
-                'name'       => $param['proName'],
-                'price'      => $param['proPrice'],
-                'start_time' => $param['proStart'],
-                'days'       => $param['proDays'],
-                'number'     => $param['proNumber'],
-                'remark'     => $param['proRemark'],
-            ]);
+	public function add() {
+		$param = $_POST;
+		$id = 0;
+		$msg = '';
+		if ($param['proAC'] == 'project') {
+			$pro = Pro::create([
+				'name' => $param['proName'],
+				'price' => $param['proPrice'],
+				'start_time' => $param['proStart'],
+				'days' => $param['proDays'],
+				'number' => $param['proNumber'],
+				'remark' => $param['proRemark'],
+			]);
 
-            $id  = $pro->id;
-            $msg = $id > 0 ? "添加成功！" : "添加失败！";
-        }
-        if ($param['proAC'] == 'bids') {
-            //{"proId":"10","bidsName":"234","bidsStart":"234","bidsDays":"234","bidsRemark":"234","proAC":"bids"}
-            //id, name, p_id, m_id, create_time, update_time
-        }
+			$id = $pro->id;
+			$msg = $id > 0 ? "添加成功！" : "添加失败！";
+		}
+		if ($param['proAC'] == 'bids') {
+			//name, p_id, m_id, create_time, update_time, remark
+			//{proId: "10", bidsName: "TEST", proManager: "22,21,20,19,18", bidsRemark: "TEST", proAC: "bids"}
+			$data = [
+				'p_id' => $param['proId'],
+				'name' => $param['bidsName'],
+				'm_id' => $param['bidsManager'],
+				'remark' => $param['bidsRemark'],
+			];
+			$id = Db::table('hy_bids')->insertGetId($data);
+			$msg = $id > 0 ? "添加成功！" : "添加失败！";
+		}
 
-        //return json(array('code' => $id, 'msg' => $msg));
-    }
+		return json(array('code' => $id, 'msg' => $msg));
+	}
 
-    public function query()
-    {
-        $list   = Pro::all();
-        $number = count($list);
+	public function query() {
+		$list = Pro::all();
+		$number = count($list);
 
-        $data = array();
+		$data = array();
 
-        foreach ($list as $key => $value) {
-            $data[] = array(
-                'proId'     => $value['id'],
-                'proName'   => $value['name'],
-                'proPrice'  => $value['price'],
-                'proStart'  => date("Y-m-d", $value['start_time']),
-                'proDays'   => $value['days'],
-                'proNumber' => $value['number'],
-                'proRemark' => $value['remark']);
-        }
+		foreach ($list as $key => $value) {
+			$data[] = array(
+				'proId' => $value['id'],
+				'proName' => $value['name'],
+				'proPrice' => $value['price'],
+				'proStart' => date("Y-m-d", $value['start_time']),
+				'proDays' => $value['days'],
+				'proNumber' => $value['number'],
+				'proRemark' => $value['remark']);
+		}
 
-        $return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
-        return json($return);
-    }
-    public function manager()
-    {
-        $param = $_GET;
-        $list   = Man::where('name','like',"%{$param['keyword']}%")->select();
-        $number = count($list);
+		$return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
+		return json($return);
+	}
+	public function manager() {
+		$param = $_GET;
+		$list = Man::where('name', 'like', "%{$param['keyword']}%")->select();
+		$number = count($list);
 
-        $data = array();
+		$data = array();
 
-        foreach ($list as $key => $value) {
-            $data[] = array(
-                'value'    => $value['id'],
-                'name'     => $value['name'],
-                'selected' => "",
-                'disabled' => "",
-            );
-        }
+		foreach ($list as $key => $value) {
+			$data[] = array(
+				'value' => $value['id'],
+				'name' => $value['name'],
+				'selected' => "",
+				'disabled' => "",
+			);
+		}
 
-        $return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
-        return json($return);
-    }
+		$return = array('code' => 0, 'msg' => '', 'count' => $number, 'data' => $data);
+		return json($return);
+	}
 }
