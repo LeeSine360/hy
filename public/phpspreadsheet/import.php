7c41fa3f3826858427ca46ed2842bc5448e36252
lee <249482000@qq.com>
2018-10-20 16:51:53 +0800

    edit

--- public/phpspreadsheet/import.php
+++ public/phpspreadsheet/import.php
@@ -7,24 +7,25 @@ $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
 $reader->setReadDataOnly(true);
 $spreadsheet = $reader->load('项目部材料合同台账.xlsx');
 
-$worksheet  = $spreadsheet->getActiveSheet();
+$worksheet = $spreadsheet->getActiveSheet();
 $highestRow = $worksheet->getHighestRow(); // 总行数
 $highestColumn = $worksheet->getHighestColumn(); // 总列数
 //$highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // e.g. 5
 
 $lines = $highestRow - 2;
 if ($lines <= 0) {
-    exit('Excel表格中没有数据');
+	exit('Excel表格中没有数据');
 }
 
 //$sql = "INSERT INTO `t_student` (`name`, `chinese`, `maths`, `english`) VALUES ";
 
-for ($row = 1; $row <= $highestRow; ++$row) {
-	for($col = 1;$col <= 12; ++$col){
-		echo $worksheet->getCellByColumnAndRow($col, $row)->getValue();
-    	
+for ($row = 2; $row <= $highestRow; ++$row) {
+	$print = '';
+	for ($col = 1; $col <= 13; ++$col) {
+		$print .= $worksheet->getCellByColumnAndRow($col, $row)->getValue() . ',';
 	}
-	echo "</br>";
+	echo rtrim($print, ",");
+	echo "<br />";
 }
 /*$sql = rtrim($sql, ","); //去掉最后一个,号
 try {
